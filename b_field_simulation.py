462799adee68ed4317510a1a2b0dabc54a61ee9b
gbrown <georg.braunbeck@googlemail.com>
2019-02-21 14:50:29 +0100

    b_field_sims: implemented auxiallry functions to one_dime_current

--- b_field_simulation.py
+++ b_field_simulation.py
@@ -6,6 +6,48 @@ import numpy as np
 from scipy.constants import mu_0, pi
 
 
-def one_dim_current(I, phi1, phi2, R):
-    B = mu_0 / (4 * pi) * I / R * (np.cos(phi1) - np.cos(phi2))
-    return B
+def one_dim_current(r, x1, x2, j, d):
+    """
+    r: location, where the B field is probed
+    x1: location of current entry
+    x2: location of current exit
+    j: current
+    d: shortest distance of infinite current line and r
+    :return: B, the magnetic field vector
+    """
+    phi1, phi2 = calc_angles(r, x1, x2)
+    B_abs = mu_0 / (4 * pi) * j / d * (np.cos(phi1) - np.cos(phi2))
+    direction = np.cross((x2 - x1), (r - x1))
+    direction_norm = direction / np.linalg.norm(direction)
+    return B_abs * direction_norm
+
+
+def calc_angles(r, x1, x2):
+    phi1 = sign(r[2]) * calc_single_angle(x2 - x1, r - x1)
+    phi2 = sign(r[2]) * calc_single_angle(x2 - x1, r - x2)
+    return phi1, phi2
+
+
+def sign(x):
+    if x >= 0:
+        sig = 1
+    else:
+        sig = -1
+    return sig
+
+
+def calc_single_angle(v1, v2):
+    return np.arccos((np.dot(v1, v2)) / (np.linalg.norm(v1) * np.linalg.norm(v2)))
+
+
+def distance_point_line(r, x1, x2):
+    numerator = np.linalg.norm(np.cross(x2 - x1, x1 - r))
+    denominator = np.linalg.norm(x2 - x1)
+    return numerator / denominator
+
+
+if __name__ == '__main__':
+    r = np.array([1, 1, 0])
+    x1 = np.array([0, 0, -1])
+    x2 = np.array([0, 0, 1])
+    print(distance_point_line(r, x1, x2))
