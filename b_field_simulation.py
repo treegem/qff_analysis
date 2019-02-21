18059b788989800e9bba6748eafd08d301970dfd
gbrown <georg.braunbeck@googlemail.com>
2019-02-21 17:07:27 +0100

    1d sim done

--- b_field_simulation.py
+++ b_field_simulation.py
@@ -6,15 +6,15 @@ import numpy as np
 from scipy.constants import mu_0, pi
 
 
-def one_dim_current(r, x1, x2, j, d):
+def one_dim_current(r, x1, x2, j):
     """
     r: location, where the B field is probed
     x1: location of current entry
     x2: location of current exit
     j: current
-    d: shortest distance of infinite current line and r
     :return: B, the magnetic field vector
     """
+    d = distance_point_line(r, x1, x2)
     phi1, phi2 = calc_angles(r, x1, x2)
     B_abs = mu_0 / (4 * pi) * j / d * (np.cos(phi1) - np.cos(phi2))
     direction = np.cross((x2 - x1), (r - x1))
@@ -47,7 +47,38 @@ def distance_point_line(r, x1, x2):
 
 
 if __name__ == '__main__':
-    r = np.array([1, 1, 0])
-    x1 = np.array([0, 0, -1])
-    x2 = np.array([0, 0, 1])
-    print(distance_point_line(r, x1, x2))
+
+    import matplotlib.pyplot as plt
+
+    xs = np.linspace(-10, 10, 100)
+    ys = np.linspace(-10, 10, 100)
+
+    bs_x = np.zeros((len(ys), len(xs)))
+    bs_y = np.zeros((len(ys), len(xs)))
+    bs_z = np.zeros((len(ys), len(xs)))
+    j = 10
+
+    x1 = np.array([0, -10, 0])
+    x2 = np.array([0, 10, 0])
+
+    for x_i, x in enumerate(xs):
+        for y_i, y in enumerate(ys):
+            b = one_dim_current(r=np.array([x, y, 1]), x1=x1, x2=x2, j=j)
+            bs_x[y_i, x_i] = b[0]
+            bs_y[y_i, x_i] = b[1]
+            bs_z[y_i, x_i] = b[2]
+
+    plt.close('all')
+    plt.imshow(bs_z, aspect='auto')
+    plt.colorbar()
+    plt.show()
+
+    plt.close('all')
+    plt.imshow(bs_x, aspect='auto')
+    plt.colorbar()
+    plt.show()
+
+    plt.close('all')
+    plt.imshow(bs_y, aspect='auto')
+    plt.colorbar()
+    plt.show()
